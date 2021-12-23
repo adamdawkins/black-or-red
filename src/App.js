5b3bce541d655d9678ad1183eb31746e49c0fe31
Adam Dawkins <adam@dragondrop.uk>
2021-12-23 13:00:31 +0000

    🏁 v0.0.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [chosenColour, setChosenColour] = useState(null);
+  const [theColour, setTheColour] = useState(null);
+
+  const deal = (event) => {
+    event.preventDefault();
+
+    const random = Math.random() * 10;
+    if (random <= 5) {
+      setTheColour("red");
+    } else {
+      setTheColour("black");
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Choose a colour :)</h1>
+      <button
+        className="text-red-500 mr-5"
+        onClick={() => setChosenColour("red")}
+      >
+        Red
+      </button>
+      <button onClick={() => setChosenColour("black")}>Black</button>
+      {chosenColour && (
+        <>
+          <p>
+            You have chosen{" "}
+            <span className={`text-${chosenColour}-500`}>{chosenColour}</span>
+          </p>
+
+          <button
+            onClick={(event) => deal(event)}
+            disabled={theColour}
+            className={["border border-black p-3 font-bold text-xl"]}
+          >
+            Deal!
+          </button>
+
+          {theColour && (
+            <>
+              <p className="text-xl">
+                The card is{" "}
+                <span className={`text-${theColour}-500`}>{theColour}</span>
+              </p>
+              {theColour == chosenColour && <p>You're the winner :)!</p>}
+              {theColour !== chosenColour && <p>You're the loser :(!</p>}
+            </>
+          )}
+        </>
+      )}
     </div>
   );
 }
